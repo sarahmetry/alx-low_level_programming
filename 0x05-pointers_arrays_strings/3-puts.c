79d6bda6992328d675f6055f15ae95201b437fe3
Sarah Metry <sarahmetry7@gmail.com>
2023-02-27 17:14:41 -0800

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+/**
+ * _puts - print
+ * @str: char
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
